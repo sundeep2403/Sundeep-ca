88bb54de76b5c2558c8465e08c5e32f7130b5427
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-25 23:56:51 +0000

    Update models.py

--- models.py
+++ models.py
@@ -4,3 +4,7 @@ from werkzeug security import generate_password_hash,check_password_hash
 class User(UserMixin):
 
   def __init__(self, id, username, email, password):
+    self.id=id
+    self.username=username
+    self.email=email
+    self.password_hash=generate_password_hash(password)
