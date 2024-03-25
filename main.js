196d3278c9e6881d9970fe717988f639400b57bd
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-25 22:54:42 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,4 @@ function searchBooks(events){
   var searchStr = event.target.value.toLowerCase();
 
   document.querySelectorAll('.container #book-list div').forEach(book) => {
+    var bookTest = book.innerText.toLowerCase();
