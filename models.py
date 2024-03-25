91872aff56089c98161465d180bd5155ae8735f8
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-25 23:58:46 +0000

    Update models.py

--- models.py
+++ models.py
@@ -10,3 +10,4 @@ class User(UserMixin):
     self.password_hash=generate_password_hash(password)
 
   def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
