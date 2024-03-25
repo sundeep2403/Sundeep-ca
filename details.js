f012c26ffcd800e59d3d33aec49acec4a7c3dd41
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-25 23:52:07 +0000

    Update details.js

--- details.js
+++ details.js
@@ -77,4 +77,11 @@ function handleBuySubmit(book) {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var quantity = document.getElementById("quantity").value;
+
+    console.log(`Details:
+        Book: ${book.Name}
+        Name: ${name}
+        Email: ${email}
+        Quantity: ${quantity}
+    `);
     
