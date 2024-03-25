365d29f3d27528fafddfe5e50e727353ebd33587
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-25 23:55:36 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,2 +1,6 @@
 from flask_login import UserMixin
 from werkzeug security import generate_password_hash,check_password_hash
+
+class User(UserMixin):
+
+  def __init__(self, id, username, email, password):
