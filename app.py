2f19bf74a6d319d0762eb9ce8ae9e940da99e92e
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-26 00:15:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from models import User
+
+users = {
+    "admin": User(1, "admin", "admin@example.com", "password")
+}
