55e68fd798de4a05b180ef9a1ce0cbccd3bc35be
sundeep2403 <65668141+sundeep2403@users.noreply.github.com>
2024-03-26 00:04:52 +0000

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,3 +4,5 @@ from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
